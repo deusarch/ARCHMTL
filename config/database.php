2c0dc2f56f7c313f4b29a1ad41e24b8f86e2ac38
deusarch <92630563+deusarch@users.noreply.github.com>
2021-12-24 00:02:40 +0700

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+/* This file is where you store database credentials. */
+$host = 'localhost'; // DB Server.
+$db = 'db_name'; // DB Name
+$user = 'db_user'; // Username.
+$password = 'password#'; // Password
+$charset = 'utf8mb4'; // DB Charset
+
+/* db_name, db_user, password#, utf8mb4 are just example, change these to your mysql login*/
+?>
